ade4d5ad56d0063a88e859f679556ad101a7bf08
sin <my.pojo@gmail.com>
2022-12-25 08:11:55 +0900

    디버깅

--- core1/src/main/kotlin/net/kotlinx/core1/string/StringTrasSupport.kt
+++ core1/src/main/kotlin/net/kotlinx/core1/string/StringTrasSupport.kt
@@ -7,7 +7,9 @@ val snakeRegex = "_[a-zA-Z]".toRegex()
 
 /** 스네이크 -> 카멜 */
 fun String.toSnakeFromCamel(): String = camelRegex.replace(this) { "_${it.value}" }.lowercase(Locale.getDefault())
+
 /** 카멜 -> 스테이크 */
 fun String.toCamelFromSnake(): String = snakeRegex.replace(this) { it.value.replace("_", "").uppercase(Locale.getDefault()) }
+
 /** 단어수 줄이기 (간이 출력용) */
-fun String.abbr(size: Int): String = if(this.length <= size) this else this.substring(0, kotlin.math.min(this.length, size))+".."
+fun String.abbr(size: Int, suff: String = ".."): String = if (this.length <= size) this else "${this.substring(0, kotlin.math.min(this.length, size))}$suff"
