0cc6b727e0b64ac5e594d7f5a6a606baef66ee27
mypoj <my.pojo@gmail.com>
2023-04-30 16:54:03 +0900

    디버깅

--- core2/src/main/kotlin/net/kotlinx/core2/file/FileGzipUtil.kt
+++ core2/src/main/kotlin/net/kotlinx/core2/file/FileGzipUtil.kt
@@ -6,22 +6,25 @@ import java.io.FileOutputStream
 import java.util.zip.GZIPOutputStream
 
 
+/**
+ * 스트림 압축은 GzipResourceSupport 참고
+ * */
 object FileGzipUtil {
 
     /** 한개 파일을 한개의 gzip 으로 압축한다. athena 등에서 csv 를 업로드 할때 사용함  */
     fun gzip(file: File): File {
-        val zip = File(file.absolutePath + ".gz")
-        GZIPOutputStream(FileOutputStream(zip)).use { gos ->
-            FileInputStream(file).use { fis ->
-                // copy file
-                val buffer = ByteArray(1024)
-                var len: Int
-                while (fis.read(buffer).also { len = it } > 0) {
-                    gos.write(buffer, 0, len)
+        return File(file.absolutePath + ".gz").also {
+            GZIPOutputStream(FileOutputStream(it)).use { gos ->
+                FileInputStream(file).use { fis ->
+                    // copy file
+                    val buffer = ByteArray(1024)
+                    var len: Int
+                    while (fis.read(buffer).also { len = it } > 0) {
+                        gos.write(buffer, 0, len)
+                    }
                 }
             }
         }
-        return zip
     }
 
 }
