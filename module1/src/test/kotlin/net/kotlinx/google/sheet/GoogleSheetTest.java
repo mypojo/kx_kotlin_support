87e6e1b4a6f13a3dcc21793f8c12ac8411d487b0
mypoj <my.pojo@gmail.com>
2023-10-01 18:36:33 +0900

    노션작업 추가..

--- module1/src/test/kotlin/net/kotlinx/google/sheet/GoogleSheetTest.java
+++ module1/src/test/kotlin/net/kotlinx/google/sheet/GoogleSheetTest.java
@@ -6,7 +6,7 @@ class GoogleSheetTest {
 //    public void mypojo_read() {
 //        File secretFile = new File("");
 //        GoogleSheet sheet = new GoogleSheet().applicationName("mypojo app").init(secretFile,secretFile.getParentFile());
-//        List<List<Object>> orgValues = sheet.load("1YtqHiyNgm2ulz7aasnyq8wLQ8o7iyjjoVSx0z4Ksdjk", "단어장");
+//        List<List<Object>> orgValues = sheet.load("xx", "단어장");
 //        TextGrid.buildFromGoogleSheet(orgValues).print();
 //        //AIzaSyDWzmKVufeyyZ5VhVhDlT405o8rn7-dJh4  my.pojo
 //    }
@@ -15,7 +15,7 @@ class GoogleSheetTest {
 //    public void nhnad_read() {
 //        File tempSecretStorage = new File("C:\\WORKSPACE\\11H11M\\core11\\core\\src\\main\\resources\\google-sheet-secret\\nhnad.11h11m\\google-sheet-secret.json");
 //        GoogleSheet sheet = new GoogleSheet().applicationName("nhnad.11h11m app").init(tempSecretStorage,tempSecretStorage.getParentFile());
-//        List<List<Object>> orgValues = sheet.load("1oCCJy1kt3x2pl2gjIpF2YJQgfYZNro_8VOEutqKWxXs", "NHN-AD IP");
+//        List<List<Object>> orgValues = sheet.load("yy", "NHN-AD IP");
 //        TextGrid.buildFromGoogleSheet(orgValues).print();
 //    }
 
