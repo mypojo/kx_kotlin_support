8b3959165e0df2017b4c5f9a8b98ee6bc12c7636
mypoj <my.pojo@gmail.com>
2023-11-26 13:48:42 +0900

    테스트 추가

--- heavy/src/test/kotlin/net/kotlinx/aws/code/CodeCommitSupportKtTest.kt
+++ heavy/src/test/kotlin/net/kotlinx/aws/code/CodeCommitSupportKtTest.kt
@@ -1,6 +1,5 @@
 package net.kotlinx.aws.code
 
-import aws.sdk.kotlin.services.codecommit.getDifferences
 import kotlinx.coroutines.runBlocking
 import net.kotlinx.aws.AwsClient
 import net.kotlinx.koin.Koins
@@ -13,6 +12,7 @@ class CodeCommitSupportKtTest : TestHeavy() {
 
     companion object {
         private const val PROJECT = "xx"
+
         init {
             MyHeavyKoinStarter.startup(PROJECT)
         }
@@ -25,19 +25,37 @@ class CodeCommitSupportKtTest : TestHeavy() {
 
         runBlocking {
 
-            val commit = aws.codeCommit.getCommit(PROJECT, "89ce3505dff8e3cf25657f208974c86b82888cee")
-            println(commit)
+
+            //히스토리 가져오는게 없다.. 왜지??
+//            val history = aws.codeCommit.listFileCommitHistory {
+//                this.repositoryName = PROJECT
+//                this.maxResults = 2
+//                this.commitSpecifier = "refs/heads/dev"
+//                this.filePath = ".gitignore"
+//            }.revisionDag
 
 
-            val paginated = aws.codeCommit.getDifferences {
-                this.repositoryName = PROJECT
-                this.afterCommitSpecifier = "89ce3505dff8e3cf25657f208974c86b82888cee"
-                this.beforeCommitSpecifier = "7ea15ce263841d035524ed1a14a73e89041996bc"
-            }
-            println(paginated.differences!!.size)
+//            println(history[0].commit!!.commitId)
+//            println(history[0].commit!!.committer)
+
+//            val paginated = aws.codeCommit.getDifferences {
+//                this.repositoryName = PROJECT
+//                this.afterCommitSpecifier = "89ce3505dff8e3cf25657f208974c86b82888cee"
+//                this.beforeCommitSpecifier = "7ea15ce263841d035524ed1a14a73e89041996bc"
+//            }
+//
+//            log.warn { "== 멀티프로젝트 체크 ===" }
+//
+//            paginated.differences!!.forEach {
+////                val file = aws.codeCommit.getFile {
+////                    this.repositoryName = PROJECT
+////                    this.filePath = it.afterBlob!!.path
+////                }
+////                println(String((file.fileContent)))
+//                println(it.afterBlob!!.path)
+//            }
 
         }
-
 
 
     }
